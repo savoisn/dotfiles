f425b0144a7531fe6f672b32f158ddaf79aebf38
savoisn <nicolas.savois@gmail.com>
2015-08-24 18:06:34 +0200

    updated tmux conf

--- install.sh
+++ install.sh
@@ -7,11 +7,11 @@ git clone https://github.com/tpope/vim-pathogen.git  ~/.vim/pathogen
 
 mkdir -p ~/.vim/autoload/
 
-ln -s ~/.vim/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+ln -fs ~/.vim/pathogen/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
 
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
 git clone https://github.com/kchmck/vim-coffee-script.git ~/.vim/bundle/vim-coffee-script/
 
-ln -s ~/.vim/vimrc/vimrc ~/.vimrc
-ln -s tmux/tmux-session ~/tmux-session
+ln -fs ~/.vim/vimrc/vimrc ~/.vimrc
+ln -fs $PWD/tmux/tmux-session ~/tmux-session
 
